0220af9ddebc20d4aa55ce3a67ed373d98b812c2
insoo <inu0616@gmail.com>
2016-06-08 14:53:06 +0900

    detail페이지 구현

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Shop, Review
 
 # Create your views here.
 def index(request):
     shop_list = Shop.objects.all()
     return render(request, 'blog/index.html', {'shop_list':shop_list})
+
+def detail(request, pk):
+    shop_detail = get_object_or_404(Shop, pk=pk)
+    shop_detail.view += 1
+    shop_detail.save()
+    return render(request, 'blog/detail.html', {'shop_detail':shop_detail})
